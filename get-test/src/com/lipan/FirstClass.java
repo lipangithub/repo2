50bc20045ecbd3da511fc721f1b3cf2b5cb77711
Pan.Li <271348929@qq.com>
2019-08-09 22:01:22 +0800

    第一次测试提交

---
+++ get-test/src/com/lipan/FirstClass.java
@@ -0,0 +1,15 @@
+package com.lipan; /**
+ * @Author: lipan
+ * @CreateTime: 2019-08-09 21:50
+ * @Description: git测试
+ */
+
+/**
+ * @author lipan
+ * @date 2019/8/9 21:50
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("git测试");
+    }
+}
